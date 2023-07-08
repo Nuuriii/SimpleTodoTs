258b2c6265ae2f9674b86274cea17722870b13aa
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-08 18:25:39 +0700

    feat: add new function and map todos

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
 
-export const Todolist = () => {
+interface Item {
+   id: number;
+   text: string;
+   completed: boolean;
+}
+
+export const Todolist: React.FC = () => {
+   const handleToggle = (id: number) => {
+      setTodos(
+         todos.map((todo) => {
+            if (todo.id === id) {
+               return { ...todo, completed: !todo.completed };
+            }
+            return todo;
+         })
+      );
+   };
+
+   const [todos, setTodos] = useState<Item[]>([
+      { id: 1, text: "Learning JS", completed: false },
+   ]);
    return (
       <div className='main-container'>
          <h1>My Todo</h1>
          <ul>
-            <li>Learning</li>
-            <li>React</li>
+            {todos.map((todo) => {
+               <li key={todo.id} onClick={() => handleToggle(todo.id)}>
+                  {todo.text}
+               </li>;
+            })}
          </ul>
          <input type='text' placeholder='add Your task!!!' />
          <button>Add</button>
